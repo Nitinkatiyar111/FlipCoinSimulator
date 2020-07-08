637b06f31f8fbcfc4d36ec8758e27440d4d28682
nitin katiyar <nkatiyar11@gmail.com>
2020-07-08 13:17:15 +0530

    couting from how much and who  won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -x
-
-	tail=1
-	head=0
-	flip=0
-	countTail=0
-	countHead=0
-		while [ $flip -le 10 ]
+tail=1
+head=0
+win=0
+countTail=0
+countHead=0
+		while [ $win -lt 1 ]
  	do
 		random=$((RANDOM % 2 ))
 		if [ $random  -eq 1 ]
@@ -16,8 +15,19 @@
 		echo "head"
       countHead=`expr $countHead + 1`
 	fi
-    flip=`expr $flip + 1`
+     if [ $countTail -eq 21 ] ||  [ $countHead -eq 21 ]
+	then
+        if [ $countTail -gt $countHead ]
+   then
+         echo "tail won"
+			winBy=`expr $countTail - $countHead`
+else
+         echo "head won"
+         winBy=`expr $countHead - $countTail`
+fi
+			win=`expr $win + 1`
+fi
 done
 echo "tail :$countTail"
 echo "head :$countHead"
-
+echo "$winBy"
