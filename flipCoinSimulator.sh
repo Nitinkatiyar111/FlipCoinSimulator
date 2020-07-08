5b19bf3f739557473c3bfb9ea8035b22b9896735
nitin katiyar <nkatiyar11@gmail.com>
2020-07-08 12:12:41 +0530

    Counting head and tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,10 +2,22 @@
 
 	tail=1
 	head=0
+	flip=0
+	countTail=0
+	countHead=0
+		while [ $flip -le 10 ]
+ 	do
 		random=$((RANDOM % 2 ))
 		if [ $random  -eq 1 ]
 	then
 		echo "tail"
+      countTail=`expr $countTail + 1`
 	else
 		echo "head"
+      countHead=`expr $countHead + 1`
 	fi
+    flip=`expr $flip + 1`
+done
+echo "tail :$countTail"
+echo "head :$countHead"
+
